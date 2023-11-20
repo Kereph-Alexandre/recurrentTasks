e40aa3c9fcdce810615a1a5c3a3726234c0e135c
Kereph <114568855+Kereph-Alexandre@users.noreply.github.com>
2023-11-20 18:40:48 +0100

    Afficher une seule tâche récurrente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+import { DisplayTasksComponent } from './display-tasks/display-tasks.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, DisplayTasksComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
 })
 export class AppComponent {
   title = 'recurringTasks';
---
+++ src/app/task/task.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RecurringTask } from '../recurring-task';
+
+@Component({
+  selector: 'app-task',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './task.component.html',
+  styleUrl: './task.component.sass',
+})
+export class TaskComponent {
+  @Input() exampleTask!: RecurringTask;
+}
