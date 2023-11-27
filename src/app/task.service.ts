457448005658d14b8706726afde680f1d0800999
Kereph <114568855+Kereph-Alexandre@users.noreply.github.com>
2023-11-27 18:28:42 +0100

    feat(service): add task service with partial GET request :construction:

---
+++ src/app/task.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { RecurringTask } from './recurring-task';
+
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TaskService {
+  private tasksUrl = 'api/tasks';
+
+  constructor(private http: HttpClient) {}
+
+  getTasks(): Observable<RecurringTask[]> {
+    return this.http.get<RecurringTask[]>(this.tasksUrl).pipe(
+      tap((_) => console.log('fetched reccurring tasks')),
+      catchError(this.handleError<RecurringTask[]>('getTasks', []))
+    );
+  }
+
+  log(arg0: string): void {
+    throw new Error('Method not implemented.');
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      console.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+}
