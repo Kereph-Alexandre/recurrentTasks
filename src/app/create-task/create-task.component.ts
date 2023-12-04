88bd67b5cedc668efb6cb10fbfd55fcf32557575
Kereph <114568855+Kereph-Alexandre@users.noreply.github.com>
2023-12-04 14:58:35 +0100

    feat: Add form to create a task :construction:

--- src/app/create-task/create-task.component.ts
+++ src/app/create-task/create-task.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RecurrentTask } from '../recurring-task';
+import { FormsModule } from '@angular/forms';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-create-task',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './create-task.component.html',
-  styleUrl: './create-task.component.sass'
+  styleUrl: './create-task.component.sass',
 })
 export class CreateTaskComponent {
-
+  modelTask: RecurrentTask = {
+    id: 0,
+    title: '',
+    description: '',
+    repeatDelay: 0,
+    creationDate: new Date(),
+    execDate: new Date(),
+    completed: false,
+  };
 }
